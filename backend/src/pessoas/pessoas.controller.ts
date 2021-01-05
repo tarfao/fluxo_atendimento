5c59f8b02c2c1ad5aba44c452ae78d5c9764378c
Ronaldo <tarfao@hotmail.com>
2021-01-05 14:29:26 -0400

    criando o sistema para buscar, e cadastrar pessoas localmente, sem usar BD

--- backend/src/pessoas/pessoas.controller.ts
+++ backend/src/pessoas/pessoas.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { PessoaService } from './shared/pessoa.service';
+import { Pessoa } from './shared/pessoa';
 
-@Controller('pessoas')
-export class PessoasController {}
+@Controller('/api/pessoas')
+export class PessoasController {
+    constructor(private pessoa: PessoaService){}
+
+    @Get()
+    async getAll(): Promise<Pessoa[]>{
+        return this.pessoa.getAll()
+    }
+
+    @Post()
+    async store(@Body() bodyPessoa): Promise<Pessoa>{
+        return this.pessoa.create(bodyPessoa)
+    }
+
+    @Post("/login")
+    async getOneLogin(@Body("login") login){
+        return this.pessoa.getByLogin(login)
+    }
+}
--- backend/src/pessoas/shared/pessoa.service.ts
+++ backend/src/pessoas/shared/pessoa.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Pessoa } from './pessoa';
 
 @Injectable()
-export class PessoaService {}
+export class PessoaService {
+    pessoas: Pessoa[] = [
+        { login: "JoaoCliente", senha: "123", tipo: "CLIENTE", comissao: null },
+        { login: "PedroCliente", senha: "123", tipo: "CLIENTE", comissao: null },
+        { login: "AtendenteMario", senha: "123", tipo: "ATENDENTE", comissao: 2.5 },
+        { login: "AtendenteRodrigo", senha: "123", tipo: "ATENDENTE", comissao: 3 },
+    ]
+
+    getAll(){
+        return this.pessoas;
+    }
+
+    getByLogin(login: string){
+        const p = this.pessoas.find(p => p.login == login)
+        return p;
+    }
+
+    create(pessoa: Pessoa){
+        this.pessoas.push(pessoa);
+        return pessoa;
+    }
+}
