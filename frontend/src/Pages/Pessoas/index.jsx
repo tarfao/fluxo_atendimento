cf1a5c714bf2b7e4822f923bb87683a749952cc6
Ronaldo <tarfao@hotmail.com>
2021-01-06 13:32:39 -0400

    criando o front com o esboço do cadastro

---
+++ frontend/src/Pages/Pessoas/index.jsx
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+import Cadastro from "./Cadastro";
+import { WrapperContainer } from "./styles";
+
+export default () => (
+    <div>
+        <WrapperContainer>
+            <Cadastro />
+        </WrapperContainer>
+    </div>
+)
---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Pessoas from './Pages/Pessoas';
+
+ReactDOM.render(
+  <Pessoas />,
+  document.getElementById('root')
+);
