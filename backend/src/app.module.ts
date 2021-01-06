c5b176e1c3d80338d280343b0f6c0c60b5e7d37e
Ronaldo <tarfao@hotmail.com>
2021-01-06 10:03:17 -0400

    removendo controllers e services app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './orm.config';
 
 @Module({
   imports: [PessoasModule, TypeOrmModule.forRoot(config)],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
